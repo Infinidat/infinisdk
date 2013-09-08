3d9588bcc615583e230f04fe6268ffd7b2d286ed
Rotem Yaari <rotemy@infinidat.com>
2013-09-08 12:53:22 +0300

    Placeholder tests

---
+++ tests/test_core/test_type_info.py
@@ -0,0 +1,13 @@
+from ..utils import TestCase
+from infinipy2.core import *
+
+class TypeInfoTest(TestCase):
+
+    def test_charsets(self):
+        self.skipTest("n/i")
+
+    def test_min_max(self):
+        self.skipTest("n/i")
+
+    def test_min_max_length(self):
+        self.skipTest("n/i")
