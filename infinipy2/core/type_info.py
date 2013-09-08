bb08148562bbc788eab26dfe53a433eef39727a1
Rotem Yaari <rotemy@infinidat.com>
2013-09-08 16:18:33 +0300

    Work on TypeInfo

--- infinipy2/core/type_info.py
+++ infinipy2/core/type_info.py
@@ -1,4 +1,50 @@
 class TypeInfo(object):
+
     def __init__(self, type, min_length=None, max_length=None, charset=None, max=None, min=None):
+        """
+        :param min_length: minimum length for parameter
+        :param max_length: maximum length for parameter
+        :param charset: sequence of characters the parameter must use
+        :param min: minimum value
+        :param max: maximum value
+        """
         super(TypeInfo, self).__init__()
         self.type = type
+        self.min_length = min_length
+        self.max_length = max_length
+        if charset is not None:
+            charset = set(charset)
+        self.charset = charset
+        self.min = min
+        self.max = max
+
+    def is_valid_value(self, value):
+        """
+        Checks if a given value is valid given the type constraints
+        """
+        result, reason = self.is_valid_value_explain(value)
+        return result
+
+    def is_valid_value_explain(self, value):
+        """
+        :rtype: A tuple of (is_valid, reason)
+        """
+        if type(value) is not self.type:
+            return (False, "Invalid type")
+
+        if self.charset and not set(value).issubset(self.charset):
+            return (False, "Invalid characters")
+
+        if self.min is not None and value < self.min:
+            return (False, "Under minimum value")
+
+        if self.max is not None and value > self.max:
+            return (False, "Exceeds maximum value")
+
+        if self.min_length is not None and len(value) < self.min_length:
+            return (False, "Too short")
+
+        if self.max_length is not None and len(value) > self.max_length:
+            return (False, "Too long")
+
+        return (True, None)
--- tests/test_core/test_type_info.py
+++ tests/test_core/test_type_info.py
@@ -3,11 +3,56 @@ from infinipy2.core import *
 
 class TypeInfoTest(TestCase):
 
+    def test_invalid_types(self):
+        self.assert_not_valid(TypeInfo(str), 2)
+        self.assert_not_valid(TypeInfo(bool), 2)
+        self.assert_not_valid(TypeInfo(int), True)
+        self.assert_not_valid(TypeInfo(str), True)
+
+        self.assert_valid(TypeInfo(str), "hello")
+        self.assert_valid(TypeInfo(str), "")
+        self.assert_valid(TypeInfo(int), 10)
+        self.assert_valid(TypeInfo(bool), True)
+
     def test_charsets(self):
-        self.skipTest("n/i")
+        type_info = TypeInfo(str, charset="abc")
+        self.assert_not_valid(type_info, "abcd")
+        self.assert_not_valid(type_info, "def")
+        self.assert_valid(type_info, "aaa")
+        self.assert_valid(type_info, "")
+        self.assert_valid(type_info, "abbbaacccb")
 
     def test_min_max(self):
-        self.skipTest("n/i")
+        self.assert_not_valid(TypeInfo(int, min=120), 119)
+        self.assert_not_valid(TypeInfo(int, max=120), 121)
+        self.assert_not_valid(TypeInfo(int, min=120, max=140), 119)
+        self.assert_valid(TypeInfo(int, min=120), 121)
+        self.assert_valid(TypeInfo(int, max=120), 29)
+        self.assert_valid(TypeInfo(int, min=120, max=140), 121)
 
     def test_min_max_length(self):
-        self.skipTest("n/i")
+        self.assert_not_valid(TypeInfo(str, max_length=5), "hello!!!")
+        self.assert_not_valid(TypeInfo(str, min_length=5), "hell")
+        self.assert_not_valid(TypeInfo(str, min_length=5, max_length=6), "hello!!!")
+        self.assert_not_valid(TypeInfo(str, min_length=5, max_length=6), "hell")
+        self.assert_valid(TypeInfo(str, min_length=5, max_length=6), "hello")
+        self.assert_valid(TypeInfo(str, min_length=5), "hello there")
+        self.assert_valid(TypeInfo(str, max_length=5), "hey")
+
+
+    def assert_not_valid(self, type_info, value):
+        result, explanation = type_info.is_valid_value_explain(value)
+        self.assertEquals(result, type_info.is_valid_value(value))
+
+        if result:
+            self.fail("{0}: Unexpectedly valid: {1!r}".format(type_info, value))
+        self.assertIsInstance(explanation, str)
+
+    def assert_valid(self, type_info, value):
+        result, explanation = type_info.is_valid_value_explain(value)
+        self.assertEquals(result, type_info.is_valid_value(value))
+
+        if not result:
+            self.fail("{0}: Unexpectedly invalid: {1!r} ({2})".format(type_info, value, explanation))
+
+        self.assertIsNone(explanation)
