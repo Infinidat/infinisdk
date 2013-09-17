e5fb735adccc1a6146353f81f796ed7101eebd9b
Rotem Yaari <rotemy@infinidat.com>
2013-09-17 13:45:26 +0300

    Update YAML dep

--- _test_build.py
+++ _test_build.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 if __name__ == '__main__':
-    modules = ["nose", "infi.unittest", "httpretty", "PyYAML"]
-    subprocess.check_call("pip install --use-mirrors {0}".format(" ".join(modules)), shell=True)
+    modules = ["nose", "infi.unittest", "httpretty", "PyYAML>=3.10"]
+    subprocess.check_call("pip install {0}".format(" ".join(repr(m) for m in modules)), shell=True)
     subprocess.check_call("python setup.py develop", shell=True)
     subprocess.check_call("nosetests -w tests", shell=True)
