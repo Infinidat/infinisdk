26b09b155fbbf51e4c6a5c1174d146345712c991
Rotem Yaari <rotemy@infinidat.com>
2013-11-03 17:20:49 +0200

    Work with izsim

--- tests/test_izbox/test_filesystem.py
+++ tests/test_izbox/test_filesystem.py
@@ -7,13 +7,14 @@ class FilesystemTest(TestCase):
 
     def setUp(self):
         super(FilesystemTest, self).setUp()
-        self.fs = self.system.objects.filesystems.get_by_id_lazy(151)
+        self.fs_name = "fs1"
+        self.fs = self.system.objects.filesystems.create(name=self.fs_name, quota=GB)
 
     def test_filesystem_get_set_quota(self):
-        self.assertEquals(self.fs.get_quota(), 2*GB)
+        self.assertEquals(self.fs.get_quota(), GB)
         self.fs.update_quota(3*GB)
 
     def test_filesystem_get_set_name(self):
-        self.assertEquals(self.fs.get_name(), "fs151")
+        self.assertEquals(self.fs.get_name(), self.fs_name)
         self.fs.update_name("new_name")
 
