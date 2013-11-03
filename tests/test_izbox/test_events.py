7e12c71c0ed55347355595eb6ed09fa312dc2b97
Rotem Yaari <rotemy@infinidat.com>
2013-11-03 13:14:47 +0200

    Fix event test

--- tests/test_izbox/test_events.py
+++ tests/test_izbox/test_events.py
@@ -1,8 +1,7 @@
 from ..utils import TestCase
 
 class EventsTest(TestCase):
-    API_SCENARIOS = ["izbox_events"]
 
     def test_get_last_events(self):
-        events = self.system.events.get_last_events(2)
-        self.assertEquals(len(events), 2)
+        events = self.system.events.get_last_events(1)
+        self.assertEquals(len(events), 1)
