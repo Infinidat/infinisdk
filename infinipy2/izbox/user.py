3ac6ca49aaeb24df747465d7dc6fc0d5a755a3c7
Rotem Yaari <rotemy@infinidat.com>
2013-11-10 15:00:47 +0200

    User support

--- infinipy2/izbox/user.py
+++ infinipy2/izbox/user.py
@@ -1,10 +1,13 @@
 from ..core import SystemObject, Field
 from ..core.system_object_utils import make_getter_updater
+from ..core.api.special_values import Autogenerate
 
 class User(SystemObject):
+
     FIELDS = [
         Field("id", is_identity=True),
-        Field("role"),
-        Field("email"),
-        Field("username"),
+        Field("role", mandatory=True, default="INFINIDAT"),
+        Field("email", mandatory=True, default=Autogenerate("user{time}@infinidat.com")),
+        Field("username", mandatory=True, default=Autogenerate("user_{ordinal}")),
+        Field("password", mandatory=True, default="12345678"),
     ]
--- tests/test_izbox/test_users.py
+++ tests/test_izbox/test_users.py
@@ -4,3 +4,6 @@ class UserTest(TestCase):
 
     def test_has_users(self):
         self.assertTrue(self.system.users.find())
+
+    def test_create_user(self):
+        user = self.system.users.create()
