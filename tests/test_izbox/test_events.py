40e8bedcd0d5d61e4bba9352ecc2f0a044eba0aa
Eran Rechter <eranr@infinidat.com>
2013-12-05 15:19:21 +0200

    add tests for custom event generation

--- tests/test_izbox/test_events.py
+++ tests/test_izbox/test_events.py
@@ -5,3 +5,10 @@ class EventsTest(TestCase):
     def test_get_last_events(self):
         events = self.system.events.get_last_events(1)
         self.assertEquals(len(events), 1)
+
+    def test_create_custom_event(self):
+        description = 'test events'
+        event_from_post = self.system.events.create_custom_event(description=description)
+        event_from_list = self.system.events.get_last_events(1)[0].get_fields()
+        self.assertEquals(event_from_list['description'], description)
+        self.assertEquals(event_from_list['code'], "CUSTOM_EVENT_INFO")
