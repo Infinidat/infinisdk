6e040e1101140d77d2b64d46ab19b842b2d42309
Rotem Yaari <rotemy@infinidat.com>
2014-03-24 11:13:04 +0200

    Fix tox tests

--- scripts/test.py
+++ scripts/test.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     modules = ["nose", "infi.unittest>=1.0.0", "pyforge"]
     subprocess.check_call("pip install {0}".format(" ".join(repr(m) for m in modules)), shell=True)
     subprocess.check_call("pip install -U git+git://gitserver.infinidat.com/qa/izsim.git@master", shell=True)
-    subprocess.check_call("python setup.py develop", shell=True)
+    subprocess.check_call("pip install -i http://pypi01.infinidat.com/simple -e .", shell=True)
     subprocess.check_call("nosetests --with-doctest", shell=True)
