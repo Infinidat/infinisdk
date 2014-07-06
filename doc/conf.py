d0e5e7c5e43c555de374a14f73b73f9526feae9a
Rotem Yaari <rotemy@infinidat.com>
2014-07-06 10:33:09 +0300

    Remove unnecessary code

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'InfiniSDK'
-copyright = u'2013, Infinidat'
+copyright = u'2014, Infinidat'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
