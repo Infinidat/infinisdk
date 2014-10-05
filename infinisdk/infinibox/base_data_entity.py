db4ce58dfacb571cb927153508c1e68d35926a6c
Rotem Yaari <rotemy@infinidat.com>
2014-10-05 10:25:49 +0300

    TESTINF-3564: Add missing docstrings

--- infinisdk/infinibox/base_data_entity.py
+++ infinisdk/infinibox/base_data_entity.py
@@ -28,12 +28,18 @@ class BaseDataEntity(InfiniBoxObject):
 
 
     def is_master(self):
+        """Returns whether or not this entity is a master entity (not a snapshot and not a clone)
+        """
         return self.get_type() == self.ENTITY_TYPES.Master
 
     def is_snapshot(self):
+        """Returns whether or not this entity is a snapshot
+        """
         return self.get_type() == self.ENTITY_TYPES.Snapshot
 
     def is_clone(self):
+        """Returns whether or not this entity is a clone
+        """
         return self.get_type() == self.ENTITY_TYPES.Clone
 
     def _create_child(self, name):
@@ -52,17 +58,23 @@ class BaseDataEntity(InfiniBoxObject):
         return child
 
     def create_clone(self, name=None):
+        """Creates a clone from this entity, if supported by the system
+        """
         if self.is_snapshot():
             return self._create_child(name)
         raise InvalidOperationException('Cannot create clone for volume/clone')
 
     def create_snapshot(self, name=None):
+        """Creates a snapshot from this entity, if supported by the system
+        """
         if self.is_snapshot():
             raise InvalidOperationException(
                 'Cannot create snapshot for snapshot')
         return self._create_child(name)
 
     def restore(self, snapshot):
+        """Restores this entity from a given snapshot object
+        """
         snapshot_id = snapshot.id
         hook_tags = self._get_tags_for_object_operations(self.system)
         restore_url = self.get_this_url_path().add_path('restore')
@@ -75,24 +87,36 @@ class BaseDataEntity(InfiniBoxObject):
         gossip.trigger_with_tags('infinidat.sdk.post_data_restore', {'source': snapshot, 'target': self}, tags=hook_tags)
 
     def get_snapshots(self):
+        """Retrieves all snapshot children of this entity
+        """
         return self.get_children(type=self.ENTITY_TYPES.Snapshot)
 
     def get_clones(self):
+        """Retrieves all clone children of this entity
+        """
         return self.get_children(type=self.ENTITY_TYPES.Clone)
 
     def get_children(self, **kwargs):
+        """Retrieves all child entities for this entity (either clones or snapshots)
+        """
         return self.find(self.system, parent_id=self.get_id(), **kwargs)
 
     def has_children(self):
+        """Returns whether or not this entity has children
+        """
         return len(self.get_children()) > 0
 
     def get_collection(self):
         return getattr(self.system, self.get_plural_name())
 
     def get_creation_time(self):
+        """Retrieves creation time for this entity
+        """
         return self.get_field("created_at", from_cache=True)
 
     @InfiniBoxObject.requires_refresh("pool")
     def move_pool(self, target_pool, with_capacity=True):
+        """Moves this entity to a new pool, optionally along with its needed capacity
+        """
         data = dict(pool_id = target_pool.get_id(), with_capacity = with_capacity)
         return self.system.api.post(self.get_this_url_path().add_path('move'), data=data)
