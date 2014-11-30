3b195efb0bfe9d0cfc1180e51c1ec9a6e0ec0a80
Roei Bar Aviv <roeib@infinidat.com>
2014-11-30 14:28:06 +0200

    Implement infinibox_cluster class

--- infinisdk/infinibox/infinibox_cluster.py
+++ infinisdk/infinibox/infinibox_cluster.py
@@ -1,3 +1,12 @@
+from ..core.system_object import  APICommandFailed, APITransportFailure
+
+import functools
+import waiting
+from contextlib import contextmanager
+from logbook import Logger
+
+_logger = Logger(__name__)
+
 class InfiniboxCluster(object):
     """Manages higher-level operations relating to the clustered nature of the system
     """
@@ -5,3 +14,53 @@ class InfiniboxCluster(object):
     def __init__(self, system):
         super(InfiniboxCluster, self).__init__()
         self.system = system
+
+    def _get_service_by_role(self, name, role):
+        services_by_name = self.system.components.services.find(name=name.lower())
+        service_by_role = [service for service in services_by_name if service.get_role() == role.upper()][0]
+        return service_by_role
+
+    def get_core_master(self):
+        return self._get_service_by_role('CORE', 'MASTER')
+
+    def get_core_secondary(self):
+        return self._get_service_by_role('CORE', 'SECONDARY')
+
+    def get_management_master(self):
+        return self._get_service_by_role('MGMT', 'MASTER')
+
+    def get_management_secondary(self):
+        return self._get_service_by_role('MGMT', 'SECONDARY')
+
+
+    @contextmanager
+    def possible_management_take_over_context(self, node):
+        management_service = node.get_management_service()
+        if management_service.is_active() and management_service.is_master():
+            management_secondary = self.get_management_secondary()
+            yield
+            self.wait_for_management_takeover(management_secondary)
+        else:
+            yield
+
+    def wait_for_management_takeover(self, new_management_master, deadline=300):
+        """During the management takeover there's a time windows (as defined in config), in which we allow API failures
+        """
+        _logger.info("Waiting for {} to become management master. deadline is {}".format(new_management_master, deadline))
+        waiting.wait(functools.partial(self.safe_is_management_cluster_master, new_management_master), timeout_seconds=deadline, sleep_seconds=5)
+
+    @staticmethod
+    def safe_is_management_cluster_master(management_service):
+        """Safe check if management is master, to handle the management master takeover
+        """
+        assert management_service.get_node().get_management_service() == management_service, \
+            "{} is expected to be management service".format(management_service)
+        result = False
+        try:
+            result = management_service.is_master()
+        except APICommandFailed:
+            pass
+        except APITransportFailure:
+            pass
+        return result
+
