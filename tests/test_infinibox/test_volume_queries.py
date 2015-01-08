dfdc2831446da2de90e76455b0f9584c65088694
Ayala Shachar <ayalas@infinidat.com>
2015-01-08 16:49:09 +0200

    Add sort by multiple fields query unittest

--- tests/test_infinibox/test_volume_queries.py
+++ tests/test_infinibox/test_volume_queries.py
@@ -1,3 +1,5 @@
+from infinisdk import Q
+
 
 def test_query_by_parent(infinibox, volume):
     snapshot = volume.create_snapshot()
@@ -12,3 +14,16 @@ def test_query_by_parent_none(infinibox, volume):
 def test_query_by_pool(infinibox, pool, volume):
     assert [volume] == list(infinibox.volumes.find(pool=pool))
     assert [] == list(infinibox.volumes.find(pool=None))
+
+def test_sort_by_multiple_fields(infinibox, pool):
+    vol_ab_thin = infinibox.volumes.create(pool=pool, name='ab', provisioning='THIN')
+    vol_bb_thin = infinibox.volumes.create(pool=pool, name='bb', provisioning='THIN')
+    vol_aa_thick = infinibox.volumes.create(pool=pool, name='aa', provisioning='THICK')
+    vol_ba_thick = infinibox.volumes.create(pool=pool, name='ba', provisioning='THICK')
+    assert set(infinibox.volumes.get_all()) == set([vol_ab_thin, vol_bb_thin, vol_aa_thick, vol_ba_thick])
+
+    get_sorted_volumes = lambda *sort_args: list(infinibox.volumes.get_all().sort(*sort_args))
+    assert get_sorted_volumes(Q.name, Q.provtype) == [vol_aa_thick, vol_ab_thin, vol_ba_thick, vol_bb_thin]
+    assert get_sorted_volumes(Q.provtype, Q.name) == [vol_aa_thick, vol_ba_thick, vol_ab_thin, vol_bb_thin]
+    assert get_sorted_volumes(-Q.provtype, Q.name) == [vol_ab_thin, vol_bb_thin, vol_aa_thick, vol_ba_thick]
+    assert get_sorted_volumes(-Q.provtype, -Q.name) == [vol_bb_thin, vol_ab_thin, vol_ba_thick, vol_aa_thick]
