3bde21912c78f4b81193b86c45adfb0dbdddf90e
Rotem Yaari <rotemy@infinidat.com>
2015-02-11 15:02:14 +0200

    Make deprecations work on methods as well

--- infinisdk/core/utils/deprecation.py
+++ infinisdk/core/utils/deprecation.py
@@ -21,6 +21,59 @@ _deprecation_logger = Logger("deprecation")
 _deprecation_locations = set()
 
 
+def forget_deprecation_locations():
+    _deprecation_locations.clear()
+
+
+class _DeprecatedFunction(object):
+
+    def __init__(self, func, message, obj=None, objtype=None):
+        super(_DeprecatedFunction, self).__init__()
+        if isinstance(func, classmethod):
+            if hasattr(func, '__func__'):
+                func = func.__func__
+            else:
+                func = func.__get__(True)
+        self._func = func
+        self._message = message
+        self._obj = obj
+        self._objtype = objtype
+        self.__name__ = func.__name__
+
+    def __call__(self, *args, **kwargs):
+        caller_location = _get_caller_location()
+        if caller_location not in _deprecation_locations:
+            warning = "{0} is deprecated.".format(self._get_func_str())
+            if self._message is not None:
+                warning += " {0}".format(self._message)
+            _deprecation_logger.warning(warning, frame_correction=+1)
+            _deprecation_locations.add(caller_location)
+        if self._obj is not None:
+            return self._func(self._obj, *args, **kwargs)
+        return self._func(*args, **kwargs)
+
+    def _get_func_str(self):
+        if self._objtype is not None:
+            return '{0}.{1}'.format(self._objtype.__name__, self._func.__name__)
+        return '{0}.{1}'.format(self._func.__module__, self._func.__name__)
+
+    def __get__(self, obj, objtype):
+        return self.bound_to(obj, objtype)
+
+    def bound_to(self, obj, objtype):
+        return _DeprecatedFunction(self._func, self._message, obj=obj, objtype=objtype)
+
+    @property
+    def __doc__(self):
+        returned = self._func.__doc__
+        if returned:  # pylint: disable=no-member
+            returned += "\n.. deprecated\n"  # pylint: disable=no-member
+            if self._message:
+                returned += "   {0}".format(self._message)  # pylint: disable=no-member
+        return returned
+
+
+
 def deprecated(func=None, message=None):
     """Marks the specified function as deprecated, and emits a warning when it's called
     """
@@ -32,24 +85,7 @@ def deprecated(func=None, message=None):
     if func is None:
         return functools.partial(deprecated, message=message)
 
-    @functools.wraps(func)
-    def new_func(*args, **kwargs):
-        caller_location = _get_caller_location()
-        if caller_location not in _deprecation_locations:
-            warning = "{func.__module__}.{func.__name__} is deprecated.".format(
-                func=func)
-            if message is not None:
-                warning += " {0}".format(message)
-            _deprecation_logger.warning(warning, frame_correction=+1)
-            _deprecation_locations.add(caller_location)
-        return func(*args, **kwargs)
-
-    if new_func.__doc__:  # pylint: disable=no-member
-        new_func.__doc__ += "\n.. deprecated\n"  # pylint: disable=no-member
-        if message:
-            new_func.__doc__ += "   {0}".format(message)  # pylint: disable=no-member
-
-    return new_func
+    return _DeprecatedFunction(func, message)
 
 
 def _get_caller_location(stack_climb=2):
---
+++ tests/test_core/test_deprecations.py
@@ -0,0 +1,123 @@
+import pytest
+import logbook
+
+from infinisdk.core.utils import deprecated
+from infinisdk.core.utils.deprecation import forget_deprecation_locations
+
+
+def test_deprecated_func_called(capture):
+    assert deprecated_func(1, 2) == 3
+
+
+def test_deprecation_message(capture):
+    deprecated_func(1, 2)
+
+    [record] = capture.records
+    assert "deprecated" in record.message
+    assert 'deprecated_func' in record.message
+
+
+def test_deprecation_with_message(capture):
+
+    @deprecated("use something else instead")
+    def func(a, b):
+        return a + b
+
+    func(1, 2)
+
+    [record] = capture.records
+    assert "use something else instead" in record.message
+    assert "func is deprecated" in record.message
+
+
+def _no_decorator(func):
+    return func
+
+
+@pytest.mark.parametrize('decorator', [_no_decorator, classmethod])
+def test_class_deprecation(capture, decorator):
+
+    class Bla(object):
+
+        @deprecated('reason')
+        @decorator
+        def func(self, a, b):
+            assert isinstance(self, Bla)
+            return a + b
+
+    assert Bla().func(2, 4) == 6
+
+    [record] = capture.records
+    assert 'Bla.func is deprecated' in record.message
+
+
+def test_deprecations_different_sources(capture):
+
+    def f():
+        deprecated_func(1, 2)
+
+    def g():
+        deprecated_func(1, 2)
+
+    f()
+    g()
+    assert len(capture.records) == 2
+
+
+def test_deprecations_same_sources(capture):
+
+    def f():
+        deprecated_func(1, 2)
+
+    f()
+    f()
+    assert len(capture.records) == 1
+
+
+def test_name_doc():
+    @deprecated
+    def some_func():
+        """docstring here"""
+        pass
+
+    assert some_func.__name__ == 'some_func'
+    assert 'docstring here' in some_func.__doc__
+
+
+def test_deprecatd_docstring():
+
+    message = "Use something else instead"
+
+    @deprecated()
+    def some_func():
+        """This is a function
+        """
+
+    @deprecated(message)
+    def other_func():
+        """This is another function
+        """
+
+    assert ".. deprecated" in some_func.__doc__
+    assert ".. deprecated\n   {0}".format(message) in other_func.__doc__
+
+
+@pytest.fixture
+def capture(request):
+    handler = logbook.TestHandler(level=logbook.WARNING)
+    handler.push_application()
+
+    @request.addfinalizer
+    def pop():
+        handler.pop_application()
+    return handler
+
+
+@deprecated
+def deprecated_func(a, b):
+    return a + b
+
+
+@pytest.fixture(autouse=True)
+def forget_locations():
+    forget_deprecation_locations()
