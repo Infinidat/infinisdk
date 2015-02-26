1c52febf08ad9aa205f94d7f8206e5934841f131
Ayala Shachar <ayalas@infinidat.com>
2015-02-26 17:42:38 +0200

    Add fields to Filesystem

--- infinisdk/infinibox/filesystem.py
+++ infinisdk/infinibox/filesystem.py
@@ -31,6 +31,7 @@ class Filesystem(BaseDataEntity):
             is_sortable=True, default=Autogenerate("fs_{uuid}")),
         Field("size", creation_parameter=True, mutable=True,
               is_filterable=True, is_sortable=True, default=GB, type=CapacityType),
+        Field("allocated", type=CapacityType, is_sortable=True, is_filterable=True),
         Field("tree_allocated", type=CapacityType, is_sortable=True, is_filterable=True),
         Field("pool", type='infinisdk.infinibox.pool:Pool', api_name="pool_id", creation_parameter=True, is_filterable=True, is_sortable=True,
               binding=RelatedObjectBinding()),
@@ -38,9 +39,13 @@ class Filesystem(BaseDataEntity):
         Field("parent", type='infinisdk.infinibox.filesystem:Filesystem', cached=True, api_name="parent_id", binding=RelatedObjectBinding('filesystems'), is_filterable=True),
         Field("provisioning", api_name="provtype", mutable=True, creation_parameter=True,
             is_filterable=True, is_sortable=True, default="THICK"),
-        Field("created_at", type=MillisecondsDatetimeType),
+        Field("created_at", type=MillisecondsDatetimeType, is_sortable=True, is_filterable=True),
+        Field("updated_at", type=MillisecondsDatetimeType, is_sortable=True, is_filterable=True),
         Field("ssd_enabled", type=bool, mutable=True, creation_parameter=True, is_filterable=True, is_sortable=True, optional=True),
         Field("write_protected", type=bool, mutable=True, creation_parameter=True, optional=True),
+        Field("depth", type=int, is_sortable=True, is_filterable=True),
+        Field("mapped", type=bool, is_sortable=True, is_filterable=True),
+        Field("has_children", type=bool),
     ]
 
     @classmethod
