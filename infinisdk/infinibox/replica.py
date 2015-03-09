6b55bfabb40cc6e07a044c1eba9936dd9e5198e6
Rotem Yaari <rotemy@infinidat.com>
2015-03-09 10:59:43 +0200

    Remove "name" field in replica

--- infinisdk/infinibox/replica.py
+++ infinisdk/infinibox/replica.py
@@ -68,7 +68,6 @@ class Replica(InfiniBoxObject):
         Field('id', type=int, is_identity=True),
         Field('link', api_name='link_id', binding=RelatedObjectBinding(
             'links'), type='infinisdk.infinibox.link:Link', creation_parameter=True),
-        Field('name', creation_parameter=True, mutable=True, is_filterable=True,
               default=Autogenerate("replica_{uuid}")),
         Field('entity_pairs', type=list, creation_parameter=True),
         Field('entity_type', type=str,
