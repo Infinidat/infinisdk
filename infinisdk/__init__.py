c9e5a28e2ad5b226770c8fc8beacdd20de2947cf
Rotem Yaari <rotemy@infinidat.com>
2015-03-12 09:35:31 +0200

    Cosmetics

--- infinisdk/__init__.py
+++ infinisdk/__init__.py
@@ -16,6 +16,7 @@ from .core.q import Q
 from .infinibox import InfiniBox
 from .izbox import IZBox
 
+_SDK_HOOK = 'infinidat.sdk.{0}'.format
 
 def _install_hooks():
     import gossip
@@ -23,37 +24,37 @@ def _install_hooks():
     # Define systems objects operation hooks
     obj_type_name = set(tag for sys in (InfiniBox, IZBox) for obj_cls in sys.OBJECT_TYPES
             for tag in obj_cls._get_tags_for_object_operations(sys))
-    for hook_name_template in ('infinidat.sdk.pre_object_{0}', 'infinidat.sdk.post_object_{0}'):
+    for hook_name_template in (_SDK_HOOK('pre_object_{0}'), _SDK_HOOK('post_object_{0}')):
         for operation in ('creation', 'deletion', 'update'):
             full_hook_name = hook_name_template.format(operation)
             gossip.define(full_hook_name, tags=obj_type_name)
-    gossip.define('infinidat.sdk.object_operation_failure', tags=obj_type_name)
+    gossip.define(_SDK_HOOK('object_operation_failure'), tags=obj_type_name)
 
-    gossip.define("infinidat.sdk.begin_fork",  tags=['infinibox', 'volume', 'filesystem'])
-    gossip.define("infinidat.sdk.cancel_fork", tags=['infinibox', 'volume', 'filesystem'])
-    gossip.define("infinidat.sdk.finish_fork", tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK("begin_fork"),  tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK("cancel_fork"), tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK("finish_fork"), tags=['infinibox', 'volume', 'filesystem'])
 
-    gossip.define('infinidat.sdk.pre_data_restore', tags=['infinibox', 'volume', 'filesystem'])
-    gossip.define('infinidat.sdk.post_data_restore', tags=['infinibox', 'volume', 'filesystem'])
-    gossip.define('infinidat.sdk.data_restore_failure', tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK('pre_data_restore'), tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK('post_data_restore'), tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK('data_restore_failure'), tags=['infinibox', 'volume', 'filesystem'])
 
-    gossip.define('infinidat.sdk.pre_creation_data_validation', tags=['infinibox', 'volume', 'filesystem'])
+    gossip.define(_SDK_HOOK('pre_creation_data_validation'), tags=['infinibox', 'volume', 'filesystem'])
 
-    gossip.define('infinidat.sdk.pre_fields_update', tags=['infinibox'])
+    gossip.define(_SDK_HOOK('pre_fields_update'), tags=['infinibox'])
 
-    gossip.define('infinidat.sdk.pre_node_phase_in', tags=['infinibox', 'node1', 'node2', 'node3'])
-    gossip.define('infinidat.sdk.post_node_phase_in', tags=['infinibox', 'node1', 'node2', 'node3'])
-    gossip.define('infinidat.sdk.node_phase_in_failure', tags=['infinibox', 'node1', 'node2', 'node3'])
-    gossip.define('infinidat.sdk.node_phased_in', tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('pre_node_phase_in'), tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('post_node_phase_in'), tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('node_phase_in_failure'), tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('node_phased_in'), tags=['infinibox', 'node1', 'node2', 'node3'])
 
-    gossip.define('infinidat.sdk.pre_node_phase_out', tags=['infinibox', 'node1', 'node2', 'node3'])
-    gossip.define('infinidat.sdk.post_node_phase_out', tags=['infinibox', 'node1', 'node2', 'node3'])
-    gossip.define('infinidat.sdk.node_phase_out_failure', tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('pre_node_phase_out'), tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('post_node_phase_out'), tags=['infinibox', 'node1', 'node2', 'node3'])
+    gossip.define(_SDK_HOOK('node_phase_out_failure'), tags=['infinibox', 'node1', 'node2', 'node3'])
 
-    gossip.define('infinidat.sdk.replica_snapshot_created', tags=['infinibox'])
+    gossip.define(_SDK_HOOK('replica_snapshot_created'), tags=['infinibox'])
 
-    gossip.define('infinidat.sdk.before_api_request')
-    gossip.define('infinidat.sdk.after_api_request')
+    gossip.define(_SDK_HOOK('before_api_request'))
+    gossip.define(_SDK_HOOK('after_api_request'))
 
     gossip.get_or_create_group('infinidat.sdk').set_strict()
 
