c7b5771f72622db95e45cb8c8e334768e441569f
Omer Gertel <omer.gertel@gmail.com>
2016-06-05 11:57:55 +0300

    This method should not be part of infinisdk. It's a better match in the notification tracking plugin

--- infinisdk/infinibox/notification_rule.py
+++ infinisdk/infinibox/notification_rule.py
@@ -1,5 +1,4 @@
 from urlobject import URLObject as URL
-from ..core.utils import DONT_CARE
 from ..core.bindings import RelatedObjectBinding
 from ..core import Field, SystemObject
 
@@ -24,17 +23,3 @@ class NotificationRule(SystemObject):
     @classmethod
     def get_type_name(cls):
         return 'notification_rule'
-
-    def does_match_event(self, event, from_cache=DONT_CARE):
-        if event.get_visibility() == 'INFINIDAT' and event.get_visibility() != self.get_target(from_cache=from_cache).get_visibility(from_cache=from_cache):
-            return False
-
-        if self.get_event_code(from_cache=from_cache) is not None and self.get_event_code(from_cache=from_cache) == event.get_code():
-            return True
-
-        if event.get_code() in self.get_include_events(from_cache=from_cache):
-            return True
-        
-        return (event.get_visibility() in self.get_event_visibility(from_cache=from_cache) and
-                event.get_level() in self.get_event_level(from_cache=from_cache) and
-                event.get_code() not in self.get_exclude_events(from_cache=from_cache))
