a074e45a8632524b6a3cd10d78eb168c756663d8
Omer Gertel <omer.gertel@gmail.com>
2016-07-11 13:35:27 +0300

    INFRADEV-7148: Fix notification rule field definition

--- infinisdk/infinibox/notification_rule.py
+++ infinisdk/infinibox/notification_rule.py
@@ -1,4 +1,5 @@
 from urlobject import URLObject as URL
+from ..core.api.special_values import Autogenerate
 from ..core.bindings import RelatedObjectBinding
 from ..core import Field, SystemObject
 
@@ -10,16 +11,17 @@ class NotificationRule(SystemObject):
 
     FIELDS = [
         Field('id', type=int, is_identity=True),
-        Field('name', mutable=True),
-        Field('event_code', type=str),
-        Field('event_level', type=list),
-        Field('target_parameters', type=dict),
-        Field('target', api_name='target_id', binding=RelatedObjectBinding('notification_targets')),
-        Field('include_events', type=list),
-        Field('exclude_events', type=list),
+        Field('name', mutable=True, creation_parameter=True, default=Autogenerate("rule_{uuid}")),
+        Field('event_code', type=str, mutable=True),
+        Field('event_level', type=list, mutable=True),
+        Field('target_parameters', type=dict, mutable=True),
+        Field('target', api_name='target_id', mutable=True, creation_parameter=True, binding=RelatedObjectBinding('notification_targets')),
+        Field('include_events', type=list, mutable=True),
+        Field('exclude_events', type=list, mutable=True),
         Field('event_visibility', type=list),
     ]
 
     @classmethod
     def get_type_name(cls):
         return 'notification_rule'
+
