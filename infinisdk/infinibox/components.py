fb892ceb35bf5f24fba2619012c3c5777529f667
Omer Gertel <omer.gertel@gmail.com>
2016-08-14 13:35:48 +0300

    TEST-5588: Improve performance of get_sub_components by only iterating on relevant fields

--- infinisdk/infinibox/components.py
+++ infinisdk/infinibox/components.py
@@ -226,7 +226,10 @@ class InfiniBoxSystemComponent(BaseSystemObject):
     get_collection = get_binder
 
     def get_sub_components(self):
-        return self.system.components.find(parent_id=self.id)
+        for field in self.fields:
+            if isinstance(field.binding, ListOfRelatedComponentBinding):
+                for component in self.get_field(field.name):
+                    yield component
 
     def refresh_cache(self):
         data = self.system.api.get(self.get_this_url_path()).get_result()
