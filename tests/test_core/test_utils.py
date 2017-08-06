c363f984061bc7536a39e2065c940f00d5d8daff
Ayala Shachar <ayalas@infinidat.com>
2017-08-06 10:08:13 +0300

    INFRADEV-9427: get_logged_in_username should handle unknown user ID

--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -1,4 +1,5 @@
 from infinisdk.core.utils import add_comma_separated_query_param
+from infinisdk.core.utils.environment import get_logged_in_username
 
 
 def test_add_comma_separated_query_param():
@@ -6,3 +7,7 @@ def test_add_comma_separated_query_param():
         "http://a.com/a/b/c", "sort", "a") == 'http://a.com/a/b/c?sort=a'
     assert add_comma_separated_query_param(
         "http://a.com/a/b/c?sort=a", "sort", "b") == 'http://a.com/a/b/c?sort=a%2Cb'
+
+
+def test_get_logged_in_username():
+    assert isinstance(get_logged_in_username(), str)
