a429a02ee1c4bc4a29171aff93f84ac4fde975b1
Ayala Shachar <ayalas@infinidat.com>
2017-08-29 11:16:32 +0300

    Fix unittest

--- tests/test_infinibox/test_fc_switches.py
+++ tests/test_infinibox/test_fc_switches.py
@@ -4,4 +4,4 @@ from ..conftest import relevant_from_version
 @relevant_from_version('3.0')
 def test_fc_swithches_query(infinibox):
     fc_switch = infinibox.fc_switches.choose()
-    assert fc_switch.get_vendor() == 'Cisco'
+    assert fc_switch.get_vendor() in ('Cisco', 'Brocade')
