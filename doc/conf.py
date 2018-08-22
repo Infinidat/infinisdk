ccf01354456d7d46b8ede6deab8b45a86c91a803
Ayala Shachar <ayalas@infinidat.com>
2018-08-22 15:49:27 +0300

    Fix pylint for doc directory

--- doc/conf.py
+++ doc/conf.py
@@ -10,8 +10,7 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
-import sys, os, pkg_resources
+import pkg_resources
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -43,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'InfiniSDK'
-copyright = u'2014, 2015, 2016, Infinidat'
+copyright = u'2014-2018, Infinidat'  # pylint: disable=redefined-builtin
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -101,9 +100,9 @@ html_theme_options = {
     "font_family": "sans-serif",
 }
 html_sidebars = {
-   '**': [
-       'about.html', 'navigation.html', 'searchbox.html',
-   ]
+    '**': [
+        'about.html', 'navigation.html', 'searchbox.html',
+    ]
 }
 
 
@@ -184,21 +183,20 @@ htmlhelp_basename = 'InfiniSDKdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'InfiniSDK.tex', u'InfiniSDK Documentation',
-   u'Infinidat', 'manual'),
+    ('index', 'InfiniSDK.tex', u'InfiniSDK Documentation', u'Infinidat', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -241,9 +239,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'InfiniSDK', u'InfiniSDK Documentation',
-   u'Infinidat', 'InfiniSDK', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'InfiniSDK', u'InfiniSDK Documentation',
+     u'Infinidat', 'InfiniSDK', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
