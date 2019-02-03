c551d74daa68012a7d363b31c0c3bfbd13a2c6e0
Ayala Shachar <ayalas@infinidat.com>
2019-02-03 11:57:28 +0200

    INFRADEV-11896: Improve NLM locks API

--- infinisdk/infinibox/nlm_lock.py
+++ infinisdk/infinibox/nlm_lock.py
@@ -1,13 +1,15 @@
-from ..core.system_object import BaseSystemObject
-from ..core.type_binder import TypeBinder
+from ..core import Field, MillisecondsDatetimeType
 from ..core.api.special_values import OMIT
 from ..core.bindings import RelatedObjectNamedBinding
-from ..core import Field, MillisecondsDatetimeType
+from ..core.system_object import BaseSystemObject
+from ..core.type_binder import TypeBinder
 from urlobject import URLObject as URL
+from vintage import deprecated
 
 
 class NlmLockTypeBinder(TypeBinder):
-    def break_lock(self, lock=OMIT, filesystem=OMIT, file_path=OMIT, client=OMIT):
+
+    def break_locks(self, lock=OMIT, filesystem=OMIT, file_path=OMIT, client=OMIT):
         data = {'file_path': file_path, 'client': client}
         data['filesystem_id'] = filesystem.get_id() if filesystem is not OMIT else OMIT
         data['lock_id'] = lock.get_id() if lock is not OMIT else OMIT
@@ -15,10 +17,15 @@ class NlmLockTypeBinder(TypeBinder):
         res = self.system.api.post(url.add_path('break'), data=data)
         return res.get_result()
 
+    @deprecated(message="Use break_locks() instead", since="134.0.0")
+    def break_lock(self, lock=OMIT, filesystem=OMIT, file_path=OMIT, client=OMIT):
+        return self.break_locks(lock=lock, filesystem=filesystem, file_path=file_path, client=client)
+
     def remove_orphan(self):
         url = URL(self.object_type.get_url_path(self.system))
         res = self.system.api.post(url.add_path('remove_orphan'))
         return res.get_result()
+
 
 class NlmLock(BaseSystemObject):
     BINDER_CLASS = NlmLockTypeBinder
@@ -35,7 +42,7 @@ class NlmLock(BaseSystemObject):
         Field("id", api_name='lock_id', type=str, is_identity=True, is_filterable=True, is_sortable=True),
         Field("lock_id", type=str, is_identity=True, is_filterable=True, is_sortable=True),
         Field("filesystem", type='infinisdk.infinibox.filesystem:Filesystem', api_name="filesystem_id",
-              is_filterable=True, is_sortable=True, binding=RelatedObjectNamedBinding(), cached=True),
+              use_in_repr=True, is_filterable=True, is_sortable=True, binding=RelatedObjectNamedBinding(), cached=True),
         Field("file_path", type=str, is_filterable=True, is_sortable=True, cached=True),
         Field("file_path_status", type=str, is_filterable=True, is_sortable=True),
         Field("client", type=str, is_filterable=True, is_sortable=True, cached=True),
