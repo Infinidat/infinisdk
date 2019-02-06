0f0b85e208d0f2ea4fb407b8017a3057a8d1e424
Lee Ballan <lballan@infinidat.com>
2019-02-06 10:20:29 +0200

    INFRADEV-11901: Add missing Tenant fields

--- infinisdk/infinibox/tenant.py
+++ infinisdk/infinibox/tenant.py
@@ -1,4 +1,4 @@
-from ..core import Field, SystemObject
+from ..core import Field, SystemObject, MillisecondsDatetimeType
 from ..core.api.special_values import Autogenerate
 from ..core.translators_and_types import MunchType
 
@@ -12,6 +12,16 @@ class Tenant(SystemObject):
         Field("visible_to_sysadmin", type=bool, default=True, creation_parameter=True, optional=True),
         Field("capacity", type=MunchType),
         Field("entity_counts", type=MunchType),
+        Field("created_at", type=MillisecondsDatetimeType, is_sortable=True, is_filterable=True),
+        Field("updated_at", type=MillisecondsDatetimeType, is_sortable=True, is_filterable=True),
+        Field("anonymous_gid", type=int, creation_parameter=True, optional=True, mutable=True, is_filterable=True,
+              is_sortable=True),
+        Field("anonymous_uid", type=int, creation_parameter=True, optional=True, mutable=True, is_filterable=True,
+              is_sortable=True),
+        Field("nfs_allow_unmapped_users", type=str, mutable=True, is_filterable=True, is_sortable=True,
+              creation_parameter=True, optional=True),
+        Field("nfs_group_policy", type=str, mutable=True, is_filterable=True, is_sortable=True,
+              creation_parameter=True, optional=True)
     ]
 
     @classmethod
