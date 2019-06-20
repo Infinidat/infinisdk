489622daed8e1865b2fea6e9e5579a1ae767d17d
Ayala Shachar <ayalas@infinidat.com>
2019-06-20 20:50:04 +0300

    INFRADEV-12482: Use itzik for run_unittests_on_simulator.py script

--- scripts/run_unittests_on_simulator.py
+++ scripts/run_unittests_on_simulator.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
-from __future__ import print_function
 import argparse
+import logging
 import os
 import subprocess
 import sys
 
-
+_logger = logging.getLogger(__name__)
+DEFAULT_LOG_LEVEL = logging.INFO
 LOCAL_PYPI_INDEX = 'https://pypi.infinidat.com/simple'
 
+
 class CommandFailed(Exception):
-    pass
+    def __init__(self, msg, rc):
+        super(CommandFailed, self).__init__(msg)
+        self.rc = rc
 
 
 def parse_args():
@@ -20,51 +24,51 @@ def parse_args():
     parser.add_argument("--branch-name", dest="branch_name", required=True)
     parser.add_argument("--infinisdk-dir", dest="sdk_dir", default=None)
     parser.add_argument("--pytest-color", dest="pytest_color", default='auto')
+    parser.add_argument("-v", action="append_const", const=-10, dest="verbosity", default=[])
+    parser.add_argument("-q", action="append_const", const=10, dest="verbosity", default=[])
     return parser.parse_args()
 
 
-def run_process(cmd, **kwargs):
-    kwargs.setdefault('shell', True)
-    proc = subprocess.Popen(cmd, **kwargs)
+def check_process(cmd, cwd=None):
+    cmd_desc = "{}$ {}".format(cwd or '', cmd)
+    _logger.info(cmd_desc)
+    proc = subprocess.Popen(cmd, cwd=cwd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = proc.communicate()
     rc = proc.returncode
+    out = out.decode('utf-8').strip()
+    err = err.decode('utf-8').strip()
+    _logger.info("Command returned RC: %s\nStdout:%s\nStderr:%s", rc, out, err)
+    if rc != 0:
+        raise CommandFailed('Command Failed with rc={}\nCommand: {}\nError:   {}'.format(rc, cmd_desc, err), rc=rc)
     return rc, out, err
 
 
-def check_process(cmd, **kwargs):
-    kwargs.setdefault('stderr', subprocess.PIPE)
-    rc, out, err = run_process(cmd, **kwargs)
-    if rc != 0:
-        raise CommandFailed('Command Failed (rc={}): {}\nError:\n{}'.format(cmd, rc, err.decode('utf-8')))
-    return out
+def main(src_dir, env_dir, python_exec, branch_name, sdk_dir, pytest_color, verbosity):
+    log_level = sum(verbosity) + DEFAULT_LOG_LEVEL
+    logging.basicConfig(stream=sys.stderr, level=log_level, format="%(message)s")
 
-
-def main(src_dir, env_dir, python_exec, branch_name, sdk_dir, pytest_color):
-    if python_exec is None:
-        python_exec = sys.executable
     if sdk_dir is None:
         sdk_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sdk_dir = os.path.abspath(os.path.expanduser(sdk_dir))
-    env_dir = os.path.abspath(os.path.expanduser(env_dir))
 
-    # Creating virtual environment
-    check_process("{} -m virtualenv {}".format(python_exec, env_dir))
+    itzik_kwargs = {
+        'src_dir': os.path.abspath(os.path.expanduser(src_dir)),
+        'env_dir': os.path.abspath(os.path.expanduser(env_dir)),
+        'python_exec': python_exec or sys.executable,
+        'branch_name': branch_name,
+    }
 
-    # Cloning simulator's repositories (infinibox_sysdefs & infinisim) + installing them
-    check_process("mkdir -p {}".format(src_dir))
-    for repo_name in ('infinibox_sysdefs', 'infinisim'):
-        check_process("git clone -b {} https://git.infinidat.com/infradev/{}.git".format(branch_name, repo_name),
-                      cwd=src_dir)
-        check_process("{}/bin/python -m pip install -i {} -e {}".format(env_dir, LOCAL_PYPI_INDEX, repo_name),
-                      cwd=src_dir)
+    # Installing infinisdk's dependencies
+    itzik_cmd = "itzik setup --src {src_dir} --env {env_dir} -i {python_exec} --bundle new-infra " + \
+        "--override-default-branch {branch_name} --stop-git-before infinisdk --clean"
+    check_process(itzik_cmd.format(**itzik_kwargs))
 
     # Installing infinisdk in the environment
     check_process("{}/bin/python -m pip install -i {} -e '.[testing]'".format(env_dir, LOCAL_PYPI_INDEX), cwd=sdk_dir)
 
     # Running unittests
     extras = '' if branch_name == 'master' else '--disable-warnings -k "not test_sphinx_doctest"'
-    rc, _, _ = run_process("{}/bin/pytest --color {} tests {}".format(env_dir, pytest_color, extras), cwd=sdk_dir)
-    return rc
+    check_process("{}/bin/pytest --color {} tests {}".format(env_dir, pytest_color, extras), cwd=sdk_dir)
 
 
 def entry_point():
@@ -72,7 +76,7 @@ def entry_point():
         kwargs = vars(parse_args())
         return main(**kwargs)
     except Exception as e:  # pylint: disable=broad-except
-        print(str(e))
+        _logger.critical("ERROR: %s", e)
         return 1
 
 
