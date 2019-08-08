a46a0dc2c87a945eabd43f9df4b472de77f5fcb2
Ayala Shachar <ayalas@infinidat.com>
2019-08-08 10:59:30 +0300

    INFRADEV-12734: Add utility for cleaner adding query parameters

--- infinisdk/core/utils/__init__.py
+++ infinisdk/core/utils/__init__.py
@@ -1,4 +1,4 @@
-from .query_utils import add_comma_separated_query_param
+from .query_utils import add_comma_separated_query_param, add_normalized_query_params
 from .python import end_reraise_context
 from .replication import handle_possible_replication_snapshot
 from sentinels import Sentinel
--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -1,4 +1,7 @@
-from infinisdk.core.utils import add_comma_separated_query_param
+import pytest
+import sys
+from infinisdk.core.api.special_values import OMIT, Autogenerate, RawValue
+from infinisdk.core.utils import add_comma_separated_query_param, add_normalized_query_params
 from infinisdk.core.utils.environment import get_logged_in_username
 
 
@@ -11,3 +14,15 @@ def test_add_comma_separated_query_param():
 
 def test_get_logged_in_username():
     assert isinstance(get_logged_in_username(), str)
+
+
+def test_add_normalized_query_params():
+    if sys.version_info[:2] < (3, 6):
+        pytest.skip("Dictionaries are not ordered")
+    assert add_normalized_query_params(
+        "http://a.com", some_key=True, other_key=False) == "http://a.com?some_key=true&other_key=false"
+    assert add_normalized_query_params(
+        "http://a.com", some_key=OMIT, other_key=RawValue(True)) == "http://a.com?other_key=True"
+    auto_gen = Autogenerate("a_{ordinal}")
+    assert add_normalized_query_params(
+        "http://a.com?pre=3", some_key=5, other_key=auto_gen) == "http://a.com?pre=3&some_key=5&other_key=a_1"
