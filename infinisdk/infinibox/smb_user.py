44c97af11ccd5135a2776e17ceff14eb56807690
David Sternlicht <dsternlicht@infinidat.com>
2021-05-27 09:11:33 +0000

    INFRADEV-15005: Remove primary_group/groups hack

--- infinisdk/infinibox/smb_user.py
+++ infinisdk/infinibox/smb_user.py
@@ -1,16 +1,13 @@
 from ..core import Field
 from ..core.bindings import (
     RelatedObjectBinding,
-    ListOfRelatedObjectBinding,
-    RelatedObjectViewBinding,
+    ListOfRelatedObjectIDsBinding
 )
 from ..core.api.special_values import Autogenerate
-from ..core.type_binder import SMBUserBinder
 from .system_object import InfiniBoxObject
 
 
 class SMBUser(InfiniBoxObject):
-    BINDER_CLASS = SMBUserBinder
     URL_PATH = "smb_users"
     FIELDS = [
         Field("id", type=int, is_identity=True, is_filterable=True, is_sortable=True),
@@ -60,28 +57,18 @@ class SMBUser(InfiniBoxObject):
             mutable=True,
         ),
         Field(
-            "primary_group_id",
-            type=int,
-            creation_parameter=True,
-            mutable=True,
-            optional=True,
-        ),
-        Field(
             "primary_group",
+            api_name="primary_group_id",
             type="infinisdk.infinibox.smb_group:SMBGroup",
-            binding=RelatedObjectViewBinding("smb_groups"),
-        ),
-        Field(
-            "group_ids",
-            type=list,
-            creation_parameter=True,
-            optional=True,
+            binding=RelatedObjectBinding("smb_groups"),
             mutable=True,
         ),
         Field(
             "groups",
+            api_name="group_ids",
             type=list,
-            binding=ListOfRelatedObjectBinding("smb_groups"),
+            binding=ListOfRelatedObjectIDsBinding("smb_groups"),
+            mutable=True,
         ),
     ]
 
@@ -92,9 +79,3 @@ class SMBUser(InfiniBoxObject):
     @classmethod
     def is_supported(cls, system):
         return system.compat.has_native_smb()
-
-    def update_primary_group(self, primary_group, **kwargs):
-        self.update_primary_group_id(primary_group.id, **kwargs)
-
-    def update_groups(self, groups, **kwargs):
-        self.update_group_ids([group.id for group in groups], **kwargs)
