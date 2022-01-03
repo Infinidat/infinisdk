a509d6af3964a243e5777e43fde1861abf323e4b
Oren Epshtain <oepshtain@infinidat.com>
2022-01-03 09:32:18 +0000

    INFRADEV-15343: Update is_replicated field to be non sortable

--- infinisdk/infinibox/vvol.py
+++ infinisdk/infinibox/vvol.py
@@ -33,7 +33,7 @@ class Vvol(SystemObject):
         Field("capacity_savings", type=CapacityType, feature_name="compression"),
         Field("used_size", api_name="used", type=CapacityType),
         Field("allocated", type=CapacityType, is_filterable=True, is_sortable=True),
-        Field("replicated", api_name="is_replicated", type=bool, is_filterable=True, is_sortable=True),
+        Field("replicated", api_name="is_replicated", type=bool, is_filterable=True, is_sortable=False),
     ]
 
     @classmethod
