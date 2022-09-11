0030d0683ae4dffe4acb6b5dc35398a10768ae18
Oren Epshtain <oepshtain@infinidat.com>
2022-09-11 12:58:19 +0000

    Backport INFRADEV-15669 into 7.1.0

--- infinisdk/infinibox/compatibility.py
+++ infinisdk/infinibox/compatibility.py
@@ -224,6 +224,9 @@ class Compatibility:
     def has_replicate_snapshots(self):
         return self.get_parsed_system_version() >= '7.1'
 
+    def has_standard_and_vvol_counts(self):
+        return self.get_parsed_system_version() >= '7.0'
+
 _VERSION_TUPLE_LEN = 5
 
 
