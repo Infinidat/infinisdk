0a712ef1e147bdb4b1628a85b6f6eea51c8a62dc
infinidat-infradev <infradev@infinidat.com>
2026-05-13 07:34:36 +0000

    Version 289.1.3 release

--- tests/test_infinibox/test_fc_switches.py
+++
@@ -1,7 +0,0 @@
-from ..conftest import relevant_from_version
-
-
-@relevant_from_version('3.0')
-def test_fc_swithches_query(infinibox):
-    fc_switch = infinibox.fc_switches.choose()
-    assert fc_switch.get_vendor() in ('Cisco', 'Brocade')
