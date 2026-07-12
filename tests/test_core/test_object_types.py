33718b293d8ae0e0e1e802dd3ff019aec2bf1d1d
infinidat-infradev <infradev@infinidat.com>
2026-07-12 11:17:56 +0000

    Version 289.1.4 release

--- tests/test_core/test_object_types.py
+++
@@ -1,3 +0,0 @@
-def test_system_types(infinibox):
-    assert infinibox.types.volume is infinibox.types.Volume
-    assert infinibox.types.volume is infinibox.volumes.object_type
--- tests/test_infinibox/test_qos_policy.py
+++
@@ -1,7 +0,0 @@
-from ..conftest import relevant_from_version
-
-@relevant_from_version('4.0')
-def test_get_assigned_entities(infinibox, volume, volume_qos_policy):
-    volume_qos_policy.assign_entity(volume)
-    assert volume_qos_policy.get_assigned_entities().to_list() == [volume]
-    assert infinibox.qos_policies.get_assigned_entities().to_list() == [volume]
